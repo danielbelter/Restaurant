9244e01da7f1f4c4df590f03b17d30d3f2fe74f0
Daniel Belter <danielbelter96@gmail.com>
2018-12-11 14:36:36 +0100

    clear session after status OK

--- src/main/java/com/app/restaurantgit/controllers/DotPayController.java
+++ src/main/java/com/app/restaurantgit/controllers/DotPayController.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/dotpay")
 public class DotPayController {
 
     @PostMapping("/dotpay")
-    public String okStatus(@RequestParam("status") String status) {
+    public String okStatus(@RequestParam("status") String status, HttpSession session) {
 
-        if (status.equals("OK,OK"))
+        if (status.equals("OK,OK")) {
+            session.removeAttribute("order");
+
             return "dotpay/zamowienieOK";
-        else if (status.equals("FAIL,FAIL"))
+        } else if (status.equals("FAIL,FAIL"))
             return "dotpay/zamowienieFAIL";
         else return "index";
 
